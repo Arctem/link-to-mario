de65c175c127bd47b0fbbb238dee319d2cf48a9d
Russell White <rarctem@gmail.com>
2021-01-08 11:31:08 -0800

    Add colors to directed graph.

--- directed.py
+++ directed.py
@@ -2,6 +2,7 @@ import os
 
 from graphviz import Digraph
 
+COLOR_ORDER = ['red', 'orange', 'yellow', 'yellowgreen', 'green', 'blue', 'purple']
 
 dot = Digraph(comment='Character Linker',
               engine='dot',
@@ -28,10 +29,13 @@ for filename in os.listdir(os.getcwd() + '/data'):
                 characters[c] = [filename]
 
 visited = set()
+visited_this_loop = set()
 seen_chars = {'Mario'}
 seen_games = set()
+distance = 0
 
 while len(seen_chars) > 0:
+    distance += 1
     while seen_chars:
         char = seen_chars.pop()
         for game in characters[char]:
@@ -39,14 +43,18 @@ while len(seen_chars) > 0:
                 seen_games.add(game)
                 dot.node(game, color='lightblue2')
                 dot.edge(char, game)
-        visited.add(char)
+        visited_this_loop.add(char)
+    visited = visited.union(visited_this_loop)
+    visited_this_loop = set()
     while seen_games:
         game = seen_games.pop()
         for char in games[game]:
             if char not in visited:
                 seen_chars.add(char)
-                dot.node(char, color='green')
+                dot.node(char, color=COLOR_ORDER[distance])
                 dot.edge(game, char)
-                visited.add(game)
+                visited_this_loop.add(game)
+    visited = visited.union(visited_this_loop)
+    visited_this_loop = set()
 
 dot.render('test-output/output.gv', view=True)
